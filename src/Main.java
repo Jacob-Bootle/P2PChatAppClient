fb885188c8f5a441765b875bbbeb1a58340e1c88
jacobbootle <1612jacob@gmail.com>
2026-07-11 17:48:52 +0100

    Allows for multiple messages to be sent to the server

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 void main() {
     try (Socket s = new Socket("localhost", 54321)) {
         DataOutputStream d = new DataOutputStream(s.getOutputStream());
-        d.writeUTF("Hello, World!");
-        d.flush();
+        Scanner reader = new Scanner(System.in);
+
+        boolean active = true;
+
+        while (active) {
+            String message = reader.next();
+            d.writeUTF(message);
+            d.flush();
+        }
     } catch (Exception e) {
         System.out.println("Client Error: " + e);
     }
